6881b2ec78de91b795b989e3b670917a6b8138bc
Tony <tr33947@gmail.com>
2021-01-28 07:58:03 +0300

    made changes

--- mycards/urls.py
+++ mycards/urls.py
@@ -1,11 +1,12 @@
-from django.conf.urls import url
+from django.conf.urls import path,include
+from django.urls import path, include
 from . import views
 from django.contrib.auth import views 
 
 
 urlpatterns = [
     path('', views.index, name='index'),
-    url(r'^logout/$', views.logout, {"next_page": '/'}),
+    path('logout/$', views.logout, {"next_page": '/'}),
     url(r'^accounts/', include('registration.backends.simple.urls')),
     
 ]
